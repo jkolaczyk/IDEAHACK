50024eb0ba0070fec7e2c1b7206c449908184d83
NIKT <jkolaczyk1@gmail.com>
2020-10-10 12:30:16 +0100

    added build folder

---
+++ client/build/precache-manifest.ef97eb2278ad9c7ffaf82b163d1a4180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc9ef00ec33ca33a26daac526d45723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99ec182935d05c00a087",
+    "url": "/static/css/main.340d7537.chunk.css"
+  },
+  {
+    "revision": "7350c6b89e765d0472bc",
+    "url": "/static/js/2.24241863.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24241863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ec182935d05c00a087",
+    "url": "/static/js/main.26296ff9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
